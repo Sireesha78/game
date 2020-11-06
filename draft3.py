2efbd080aaf76c61fa1cbb8847f115f438b9c2a0
pragya <pragyajha2812@msitprogram.net>
2020-11-07 00:37:26 +0530

    score added

--- draft3.py
+++ draft3.py
@@ -8,15 +8,18 @@ def appStarted(app):
     app.colors = []                         #will contain colors of cells
     initFillGrid(app)                       #initial random coloring function
     app.selected = []                       #keeps account of selected cells
-    app.score = 0
+
+score = 0
 
 def initFillGrid(app):
+    global score
     colors = ['red', 'blue', 'green', 'yellow']             #list of desired colors
     for _ in range(app.rows):
         color = random.choices(colors, k=app.cols)          
         app.colors.append(color)
     while(checkMatches(app)!=0):
         pass
+    score = 0 
 
 def getCellBounds(app, row, col):                           #takes row and col, then returns top-left.. 
     gridWidth  = app.width - 2*app.margin                   #..and bottom-right coordinates of a cell
@@ -35,6 +38,7 @@ def getCell(app, x, y):                                     #finds cell for give
         col = int((x - app.margin) / cellWidth)
         return (row, col)
 
+
 def mousePressed(app, event):                               #this will happen on a mouse click
     (row, col) = getCell(app, event.x, event.y)                 #get row and col for location of click
     app.selected.append((row, col))
@@ -43,17 +47,24 @@ def mousePressed(app, event):                               #this will happen on
         app.selected = []                                       #after swapping set app.selected as empty
     while(checkMatches(app)!=0):
         pass
-
+    app.score = score
 
 def swapCells(app):
+    global score
     r1 = app.selected[0][0]
     c1 = app.selected[0][1]
     r2 = app.selected[1][0]
     c2 = app.selected[1][1]
     if (abs(r1-r2)==1 and c1==c2) or (r1==r2 and abs(c1-c2)==1):        #if cells are adjacent, swap their colors
         (app.colors[r1][c1], app.colors[r2][c2]) = (app.colors[r2][c2], app.colors[r1][c1])
-        if not validMatch5(app, r1, c1, r2, c2) and not validMatch3(app, r1, c1, r2, c2):       #if move is not valid, restore..
+        match3 = validMatch3(app, r1, c1, r2, c2)
+        match5 = validMatch5(app, r1, c1, r2, c2)
+        if not match3 and not match5:       #if move is not valid, restore..
             (app.colors[r1][c1], app.colors[r2][c2]) = (app.colors[r2][c2], app.colors[r1][c1]) #..original colors
+        if match5:
+            score += 2
+        elif match3:
+            score += 1
 
 def validMatch3(app, row1, col1, row2, col2):                           #check if swap is valid match 3 move
     return eliminateMatch3(app, row1, col1) or eliminateMatch3(app, row2, col2)
@@ -81,12 +92,10 @@ def eliminateMatch3(app, row, col):                                     #finds a
     if row<app.rows-2:
         coordinates = [(row,col), (row+1, col), (row+2, col)]
         if eliminateCells(app, coordinates):
-            # app.score += 1
             return True
     if row>1:
         coordinates = [(row-2,col), (row-1, col), (row, col)]
         if eliminateCells(app, coordinates):
-            # app.score += 1
             return True
     return False
 
@@ -94,12 +103,10 @@ def eliminateMatch5(app, row, col):
     if col>1 and col<app.cols-2:
         coordinates = [(row, col-2),(row,col-1), (row, col), (row, col+1),(row, col+2)]
         if eliminateCells(app, coordinates):
-            # app.score += 2
             return True
     if row>1 and row<app.rows-2:
         coordinates = [(row-2, col),(row-1,col), (row, col), (row+1, col),(row+2, col)]
         if eliminateCells(app, coordinates):
-            # app.score += 2
             return True  
 
 def eliminateCells(app, coordinates):                               #sets cells to none if matched
@@ -133,13 +140,20 @@ def new_cell(app):
                 app.colors[row][col] = random.choice(color)
 
 def checkMatches(app):
+    global score
     c = 0
     for row in range(app.rows):
         for col in range(app.cols):
-            if eliminateMatch5(app, row ,col) or eliminateMatch3(app, row, col):
+            if eliminateMatch5(app, row, col): 
                 check_gaps(app)
                 new_cell(app)
                 c += 1
+                score += 2
+            elif eliminateMatch3(app, row, col):
+                check_gaps(app)
+                new_cell(app)
+                c += 1
+                score += 2
     return c
 
 def redrawAll(app, canvas):                                 #this will be seen on output screen
@@ -153,5 +167,6 @@ def redrawAll(app, canvas):                                 #this will be seen o
                 border = 'black'
                 thick = 1
             canvas.create_rectangle(x0, y0, x1, y1, fill=app.colors[row][col], outline=border, width= thick)
+    print(score)
 
 runApp(width=400, height=400)
