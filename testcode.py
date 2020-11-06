183a09ca4f7b12729653bbb1340094f64d8192f4
pragya <pragyajha2812@msitprogram.net>
2020-11-07 01:18:22 +0530

    nothing

--- testcode.py
+++ testcode.py
@@ -53,44 +53,6 @@ check_gaps(colorGrid)
 print("---------------------")
 printGrid(colorGrid)
 
-import random
-
-rows = 10
-cols = 10
-
-colorGrid = []
-colors = ['red', 'blue', 'green', 'yellow'] 
-
-for _ in range(rows):
-    color = random.choices(colors, k=cols)
-    colorGrid.append(color)
-
-# for row in range(rows):
-#     col = 0
-#     while(col<cols-2):
-#         if colorGrid[row][col] == colorGrid[row][col+1] == colorGrid[row][col+2]:
-#             colorGrid[row][col] = None
-#         else:
-#             col += 1
-
-# for col in range(cols):
-#     row = 0
-#     while(row<rows-2):
-#         if colorGrid[row][col] == colorGrid[row+1][col] == colorGrid[row+2][col]:
-#             colorGrid[row][col] = None
-#         else:
-#             row += 1
-
-def printGrid(grid):
-    for r in grid:
-        for c in r:
-            if c == None:
-                c = "None"
-            print(c.rjust(8), end=" ")
-        print()
-
-printGrid(colorGrid)
-
 
 # l = [1, 1, 1, 1, 2, 3, 4, 5, 3, 3]
 # # print(l[-2:].count(3))
